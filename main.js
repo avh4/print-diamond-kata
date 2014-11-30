8f257460fd4949c309fb58567759382e6e922ace
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-29 22:18:43 -0800

    Third test passes

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 
 function printDiamond(n) {
-  if (n > 1) {
-    spaces = " ";
-  } else {
-    spaces = "";
+  if (n == 1) {
+    console.log("A");
   }
-  console.log(spaces + "A");
-  if (n > 1) {
-    console.log("B" + spaces + "B");
-    console.log(spaces + "A");
+  if (n == 2) {
+    console.log(" A");
+    console.log("B B");
+    console.log(" A");
+  }
+  if (n == 3) {
+    console.log("  A");
+    console.log(" B B");
+    console.log("C   C");
+    console.log(" B B");
+    console.log("  A");
   }
 }
 
 printDiamond(1);
 printDiamond(2);
+printDiamond(3);
