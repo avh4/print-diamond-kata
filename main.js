88c88c1448b4675f55f26d99726561cf094b4ef3
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-29 22:15:25 -0800

    Second test passes

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
 function printDiamond(n) {
-  console.log("A");
+  if (n > 1) {
+    spaces = " ";
+  } else {
+    spaces = "";
+  }
+  console.log(spaces + "A");
+  if (n > 1) {
+    console.log("B" + spaces + "B");
+    console.log(spaces + "A");
+  }
 }
 
 printDiamond(1);
+printDiamond(2);
