a28f3b8d76bdde965542456bbeb9266cd8454cc7
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-29 22:40:17 -0800

    Remove special cases

--- main.js
+++ main.js
@@ -4,26 +4,18 @@ function spaces(n) {
 }
 
 function printDiamond(n) {
-  if (n == 1) {
-    console.log(spaces(n-1) + "A");
+  console.log(spaces(n-1) + "A");
+  j = 1;
+  for (i=n-2, c=1; i >= 0; i--, j+=2, c++) {
+    letter = String.fromCharCode(65 + c)
+    console.log(spaces(i) + letter + spaces(j) + letter);
   }
-  if (n == 2) {
-    console.log(spaces(n-1) + "A");
-    console.log("B" + spaces(1) + "B");
-    console.log(spaces(n-1) + "A");
+  j -= 4;
+  for (i=1, c=n-2; i <= n-2; i++, j-=2, c--) {
+    letter = String.fromCharCode(65 + c)
+    console.log(spaces(i) + letter + spaces(j) + letter);   
   }
-  if (n == 3 || n == 4) {
-    console.log(spaces(n-1) + "A");
-    j = 1;
-    for (i=n-2, c=1; i >= 0; i--, j+=2, c++) {
-      letter = String.fromCharCode(65 + c)
-      console.log(spaces(i) + letter + spaces(j) + letter);
-    }
-    j -= 4;
-    for (i=1, c=n-2; i <= n-2; i++, j-=2, c--) {
-      letter = String.fromCharCode(65 + c)
-      console.log(spaces(i) + letter + spaces(j) + letter);   
-    }
+  if (n > 1) {
     console.log(spaces(n-1) + "A");
   }
 }
